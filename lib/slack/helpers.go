63ff4109b6242e25fe9ce67ef5447f275a3c460c
Dimitris <dstrants@gmail.com>
2022-01-02 17:50:23 +0200

    Adds Hackernews Integration (#6)
    * 🚧  Prepares env for hackernews integration
    
    * 🚧  Introduces hackernews models
    
    * 🚧  Introduces hackernews functionality
    
    * ✨  Slack helper that formats the hn message
    
    * ✨  aligns slack logic to covid as well
    
    * 🔖  Bumps to next minor version

--- lib/slack/helpers.go
+++ lib/slack/helpers.go
@@ -1,22 +1,50 @@
 package slack
 
-import "github.com/slack-go/slack"
+import (
+	"fmt"
+	"with_coffee/lib/hackernews"
+
+	"github.com/slack-go/slack"
+)
 
 // Instantiates a new Message that will send all info
 func InitWithCoffeeMessage() slack.Message {
 	header := slack.NewHeaderBlock(slack.NewTextBlockObject("plain_text", ":wave: :coffee: :sunny: Good Morning! Find your daily news below", false, false))
-	return slack.NewBlockMessage(header)
+	message := slack.NewBlockMessage(header)
+	message.Blocks.BlockSet = append(message.Blocks.BlockSet, slack.NewDividerBlock())
+	return message
 }
 
 // Helper that appends all covid data to the parent message as a separate block.
-func CovidMessageBlock(blocks []string) (*slack.SectionBlock, *slack.SectionBlock) {
-	headerText := slack.NewTextBlockObject("mrkdwn", ":microbe: *Covid Cases Stats*", false, false)
+func CovidMessageBlock(blocks []string, message slack.Message) slack.Message {
+	headerText := slack.NewTextBlockObject("mrkdwn", ":microbe: *COVID CASES STATS* :microbe:", false, false)
 	headerSection := slack.NewSectionBlock(headerText, nil, nil)
+
+	message.Blocks.BlockSet = append(message.Blocks.BlockSet, headerSection)
 
 	fields := make([]*slack.TextBlockObject, 0)
 	for _, msg := range blocks {
 		fields = append(fields, slack.NewTextBlockObject("mrkdwn", msg, false, false))
 	}
 
-	return headerSection, slack.NewSectionBlock(nil, fields, nil)
+	message.Blocks.BlockSet = append(message.Blocks.BlockSet, slack.NewSectionBlock(nil, fields, nil))
+	message.Blocks.BlockSet = append(message.Blocks.BlockSet, slack.NewDividerBlock())
+
+	return message
+}
+
+// Helper that appends all hackenews stories to the parent message as a separate block.
+func HackerNewsMessageBlock(stories []hackernews.Story, message slack.Message) slack.Message {
+	headerText := slack.NewTextBlockObject("mrkdwn", ":newspaper: *HACKER NEWS STORIES* :newspaper:", false, false)
+	headerSection := slack.NewSectionBlock(headerText, nil, nil)
+
+	message.Blocks.BlockSet = append(message.Blocks.BlockSet, headerSection)
+
+	for i, story := range stories {
+		msg := fmt.Sprintf("%d. <%s|%s> \n\t :man-pouting: _%s_ - :star-struck: %d", i+1, story.URL, story.Title, story.By, story.Score)
+		message.Blocks.BlockSet = append(message.Blocks.BlockSet, slack.NewSectionBlock(slack.NewTextBlockObject("mrkdwn", msg, false, false), nil, nil))
+	}
+	message.Blocks.BlockSet = append(message.Blocks.BlockSet, slack.NewDividerBlock())
+
+	return message
 }
--- main.go
+++ main.go
@@ -2,23 +2,24 @@ package main
 
 import (
 	"with_coffee/lib/covid"
+	"with_coffee/lib/hackernews"
 	"with_coffee/lib/slack"
-
-	slackApi "github.com/slack-go/slack"
 )
 
-const Version = "0.2.4"
+const Version = "0.3.0"
 
 func main() {
-	covid.ImportAllCountriesCases()
+	// Initialized slack message
 	message := slack.InitWithCoffeeMessage()
 
 	// Covid Stats
-	covidHeader, covidStats := slack.CovidMessageBlock(covid.LoadCovidCases())
-	message.Blocks.BlockSet = append(message.Blocks.BlockSet, covidHeader)
-	message.Blocks.BlockSet = append(message.Blocks.BlockSet, covidStats)
-	message.Blocks.BlockSet = append(message.Blocks.BlockSet, slackApi.NewDividerBlock())
+	covid.ImportAllCountriesCases()
+	covidCases := covid.LoadCovidCases()
+	message = slack.CovidMessageBlock(covidCases, message)
+
+	// HackerNews Stories
+	stories := hackernews.ImportStories()
+	message = slack.HackerNewsMessageBlock(stories, message)
 
 	slack.SendMultiBlockMessage(message)
-
 }
