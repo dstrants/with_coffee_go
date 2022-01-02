702535edbaafbf78b36007b6c83ee876af46ab00
Dimitris <dimitris.strantsalis@workable.com>
2022-01-02 17:38:31 +0200

    🚧  Prepares env for hackernews integration

--- main.go
+++ main.go
@@ -1,24 +1,25 @@
 package main
 
 import (
-	"with_coffee/lib/covid"
+	"with_coffee/lib/hackernews"
 	"with_coffee/lib/slack"
-
-	slackApi "github.com/slack-go/slack"
 )
 
 const Version = "0.2.4"
 
 func main() {
-	covid.ImportAllCountriesCases()
+	// covid.ImportAllCountriesCases()
 	message := slack.InitWithCoffeeMessage()
 
-	// Covid Stats
-	covidHeader, covidStats := slack.CovidMessageBlock(covid.LoadCovidCases())
-	message.Blocks.BlockSet = append(message.Blocks.BlockSet, covidHeader)
-	message.Blocks.BlockSet = append(message.Blocks.BlockSet, covidStats)
-	message.Blocks.BlockSet = append(message.Blocks.BlockSet, slackApi.NewDividerBlock())
+	// // Covid Stats
+	// covidHeader, covidStats := slack.CovidMessageBlock(covid.LoadCovidCases())
+	// message.Blocks.BlockSet = append(message.Blocks.BlockSet, covidHeader)
+	// message.Blocks.BlockSet = append(message.Blocks.BlockSet, covidStats)
+	// message.Blocks.BlockSet = append(message.Blocks.BlockSet, slackApi.NewDividerBlock())
+
+	stories := hackernews.ImportStories()
+
+	message = slack.HackeNewsMessageBlock(stories, message)
 
 	slack.SendMultiBlockMessage(message)
-
 }
